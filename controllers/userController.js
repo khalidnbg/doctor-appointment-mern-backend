49668b20f7dd76fd9eff731403ec602e520eeccf
khalidnbg <khalidnabgaoui4@gmail.com>
2024-11-10 01:18:42 +0100

    cancel an appointment backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -195,6 +195,41 @@ const listAppointment = async (req, res) => {
   }
 };
 
+// api to cancel appointment
+const cancelAppointment = async (req, res) => {
+  try {
+    const { userId, appointmentId } = req.body;
+
+    const appointmentData = await appointmentModel.findById(appointmentId);
+
+    // check appointment user
+    if (appointmentData.userId !== userId) {
+      return res.json({ success: false, message: "Unauthorized action" });
+    }
+
+    await appointmentModel.findByIdAndUpdate(appointmentId, {
+      cancelled: true,
+    });
+
+    // releasing doctor slot
+    const { docId, slotDate, slotTime } = appointmentData;
+
+    const doctorData = await doctorModel.findById(docId);
+
+    let slots_booked = doctorData.slots_booked;
+
+    slots_booked[slotDate] = slots_booked[slotDate].filter(
+      (e) => e !== slotTime
+    );
+
+    await doctorModel.findByIdAndUpdate(docId, { slots_booked });
+
+    res.json({ success: true, message: "Appointment cancelled" });
+  } catch (error) {
+    res.json({ success: false, error: error.message });
+  }
+};
+
 export {
   registerUser,
   loginUser,
@@ -202,4 +237,5 @@ export {
   updateProfile,
   bookAppointment,
   listAppointment,
+  cancelAppointment,
 };
