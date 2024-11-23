a49be9a70a9696ac974573e93a4db15776aeb3ea
khalidnbg <khalidnabgaoui4@gmail.com>
2024-11-23 22:51:08 +0100

    add login for doctor logic

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,3 +1,6 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import doctorModel from "../models/doctorModel.js";
 
 const changeAvailability = async (req, res) => {
@@ -25,4 +28,55 @@ const doctorList = async (req, res) => {
   }
 };
 
-export { changeAvailability, doctorList };
+const loginDoctor = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const doctor = await doctorModel.findOne({ email });
+    if (!doctor) {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, doctor.password);
+    if (isMatch) {
+      const token = jwt.sign({ id: doctor._id }, process.env.JWT_SECRET);
+
+      res.json({ success: true, token });
+    } else {
+      return res.json({ success: false, message: "Invalid doc credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { changeAvailability, doctorList, loginDoctor };
+
+name;
+("Dr. Richard James");
+email;
+("doc1@gmail.com");
+password;
+("$2b$10$cYSEot3RG1uBGpingg18tOml4ppYTTgwILBIKYcEHwiyRg0wMbEzG");
+image;
+("https://res.cloudinary.com/deqqp45mt/image/upload/v1730827935/mvxgwdxv…");
+speciality;
+("General physician");
+degree;
+("MBBS");
+experience;
+("4 Year");
+about;
+("Dr. Davis has a strong commitment to delivering comprehensive medical …");
+available;
+false;
+fees;
+60;
+
+address;
+Object;
+date;
+1730827946079;
+
+slots_booked;
+Object;
