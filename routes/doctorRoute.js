5a8540ab4751a75f7edef1873a0e61f5bbc19549
khalidnbg <khalidnabgaoui4@gmail.com>
2024-11-26 23:28:12 +0100

    profile and update profile logic

--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -6,7 +6,9 @@ import {
   appointmentsDoctor,
   doctorDashboard,
   doctorList,
+  doctorProfile,
   loginDoctor,
+  updateDoctorProfile,
 } from "../controllers/doctorController.js";
 import authDoctor from "../middlewares/authDoctor.js";
 
@@ -18,5 +20,7 @@ doctorRouter.get("/appointments", authDoctor, appointmentsDoctor);
 doctorRouter.post("/complete-appointment", authDoctor, appointmentComplete);
 doctorRouter.post("/cancel-appointment", authDoctor, appointmentCancel);
 doctorRouter.get("/dashboard", authDoctor, doctorDashboard);
+doctorRouter.post("/profile", authDoctor, doctorProfile);
+doctorRouter.post("/update-profile", authDoctor, updateDoctorProfile);
 
 export default doctorRouter;
